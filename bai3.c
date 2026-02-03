7810c99c2a5fc01f64d61b0395421ef13d6669e7
letanthanh17072007-art <letanthanh17072007@gmail.com>
2026-02-03 20:30:47 +0700

    lê tấn thành ps48491 nop lap 5

--- bai3.c
+++ bai3.c
@@ -1,23 +1,33 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
- *****************************************************************************/
-
-//  BÀI 3: XÂY DỰNG HÀM HOÁN VỊ 
-//  Input: Nhập vào từ bàn phím 2 giá trị a,b 
-//  Output: hoán vị 2 giá trị của a và b 
-//  Hướng dẫn thực hiện: 
-//  Xây dựng hàm hoán vị sử dụng con trỏ và gọi hàm theo tham chiếu 
+ * Họ và tên: [Lê Tấn Thành]
+ * MSSV:      [PS48491]
+ * Lớp:       [COM108-C21302]
+ ******************************************************************************/
 
 #include <stdio.h>
 
-//Tạo hàm 
-
-int main() {
-
-    // //Gọi hàm trong hàm main 
-    
-    return 0;
+// Hàm hoán vị sử dụng con trỏ (pointers)
+void hoanVi(int *a, int *b) {
+    int temp = *a; // Bước 1: Giữ giá trị của a vào biến tạm
+    *a = *b;       // Bước 2: Gán giá trị của b cho a
+    *b = temp;     // Bước 3: Gán giá trị tạm (a cũ) cho b
 }
 
+int main() {
+    int x, y;
+
+    // Nhập dữ liệu từ bàn phím
+    printf("Nhập số a: ");
+    scanf("%d", &x);
+    printf("Nhập số b: ");
+    scanf("%d", &y);
+
+    printf("\nTrước khi hoán vị: a = %d, b = %d\n", x, y);
+
+    // Gọi hàm theo tham chiếu (truyền địa chỉ của x và y)
+    hoanVi(&x, &y);
+
+    printf("Sau khi hoán vị:  a = %d, b = %d\n", x, y);
+
+    return 0;
+}
